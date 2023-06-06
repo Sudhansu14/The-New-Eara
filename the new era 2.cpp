019448a7cc196ab3b1a7200f986c7300b519ecfa
Sudhansu Patra <130463585+Sudhansu14@users.noreply.github.com>
2023-06-06 17:47:12 +0530

    the new era 2.cpp

--- the new era 2.cpp
+++ the new era 2.cpp
@@ -1,14 +1,44 @@
 #include<iostream>
-#include<stdio.h>
+
 using namespace std;
 
-class distance
+class Distance
 {
-	public;
+	public:
 	int feet,inch,sum;
 };
 
 int main()
 {
-	distance 
+	Distance d1,d2,sum;
+	
+	cout<<"enter the 1 distance";
+	cout<<"enter the feet=";
+	cin>>d1.feet;
+	cout<<"ente the 1 inch=";
+	cin>>d1.inch;
+	
+	cout<<"enter the 2 distance";
+	cout<<"enter the feet=";
+	cin>>d2.feet;
+	cout<<"ente the 2 inch=";
+	cin>>d2.inch;
+	
+	sum.feet=d1.feet+d2.feet;
+	sum.inch=d1.inch+d2.inch;
+	
+	while(sum.inch>=12)
+	{
+		sum.feet++;
+		sum.inch-=12;
+	}
+	
+	cout<<"feet"<<sum.feet<<"inch="<<sum.inch;
+	
+	
+	
+	
+	
+	
 }
+
