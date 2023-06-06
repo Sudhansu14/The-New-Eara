bace35b5b5d35168ef641444cff6369ab21e23de
Sudhansu Patra <130463585+Sudhansu14@users.noreply.github.com>
2023-06-06 18:02:29 +0530

    Add files via upload

---
+++ Name eara 5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class Bikes
+{
+	public:
+		
+	char company__name[100];
+	char modal__name[100];	
+};
+int main()
+{
+	Bikes b1,b2;
+	
+	strcpy(b1.company__name,"kavasaki Ninja");
+	strcpy(b1.modal__name,"ZX10R");
+	strcpy(b2.company__name,"BMW");
+	strcpy(b2.modal__name,"GS150");
+	
+	cout<<"company name="<<b1.company__name<<endl;
+	cout<<"modal name="<<b1.modal__name<<endl;
+	cout<<"company name="<<b1.company__name<<endl;
+	cout<<"modal name="<<b1.modal__name<<endl;
+	
+	
+	return 0;
+}
